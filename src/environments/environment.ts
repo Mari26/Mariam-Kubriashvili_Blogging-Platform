f6e6639526c80ca6c28eff32c17fd7d96774c2e3
Mari26 <36435752+Mari26@users.noreply.github.com>
2025-05-16 22:23:23 +0400

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,10 +2,10 @@ export const environment = {
       production: false,
       firebase: { // This 'firebase' key is a common convention
         apiKey: "firebase_Api_key", // Your actual API key
-        authDomain: "personal-blog-site-853c2.firebaseapp.com",
-        projectId: "personal-blog-site-853c2",
-        storageBucket: "personal-blog-site-853c2.firebasestorage.app",
-        messagingSenderId: "947036467098",
+        authDomain: "authDomain",
+        projectId: "projectId",
+        storageBucket: "storageBucket",
+        messagingSenderId: "messagingSenderId",
         appId: "APP_ID"
         // measurementId: "YOUR_MEASUREMENT_ID" // Optional, if you have it
       }
