bda0e0eb5447251fc56b3e5979eb5f0f4f638467
Mari26 <36435752+Mari26@users.noreply.github.com>
2025-05-16 22:08:19 +0400

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@ export const environment = {
         apiKey: "AIzaSyBkZ4PnsfRovxkbbZf18It5RA5fW4CTPHE", // Your actual API key
         authDomain: "authDomain",
         projectId: "projectId",
-        storageBucket: "personal-blog-site-853c2.firebasestorage.app",
+        storageBucket: "storageBucket",
         messagingSenderId: "947036467098",
         appId: "APP_ID"
         // measurementId: "YOUR_MEASUREMENT_ID" // Optional, if you have it
